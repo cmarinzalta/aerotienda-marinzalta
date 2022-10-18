096a617cdf345e828a6408707893b9d0a5c04f5c
Candela Marinzalta <candela.marinzalta@gmail.com>
2022-10-18 14:16:27 -0300

    cambios en las card

--- src/componentes/Cards.jsx
+++ src/componentes/Cards.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./sass/cards.scss";
 export const Cards = (props) => {
     console.log(props)
     return (
--- src/componentes/ItemDetail/itemDetail.jsx
+++ src/componentes/ItemDetail/itemDetail.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 
-export const ItemDetail = ({productos = {
-    id:"1" , 
-    nombre:"Lego Air Plane" ,
-     stock: "1" ,
-     categoria: "Coleccion" ,
-     precio:"25000", 
-     descuento:"0", 
-     img:"src/componentes/img/legoplane.png",
-}}) => {
+export const ItemDetail = ({ productos = {
+  id: "1",
+  nombre: "Lego Air Plane",
+  stock: "1",
+  categoria: "Coleccion",
+  precio: "25000",
+  descuento: "0",
+  img: "src/componentes/img/legoplane.png",
+} }) => {
   return (
-    <div>itemDetail</div>
+    <div>
+      <div className="card mb-3" style="max-width: 540px;">
+        <div clasNames="row g-0">
+          <div className="col-md-4">
+            <img
+              src={props.producto.imagen}
+              class="img-fluid rounded-start" 
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{props.producto.nombre}</h5>
+              <p className="card-text">stock: {props.producto.stock}</p>
+              <p className="card-text"><small className="text-muted">precio: $ {props.producto.precio}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
